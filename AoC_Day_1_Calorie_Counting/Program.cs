741cc97218bbe3324f3327eeb4b66a988bf111e1
Iulian Caluian <iulian.caluian@gi.local>
2022-12-14 15:57:07 +0200

    day_1

---
+++ AoC_Day_1_Calorie_Counting/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string[] lines = File.ReadAllLines("input.txt");
+
+int maxCal1 = 0;
+int maxCal2 = 0;
+int maxCal3 = 0;
+
+int maxPoz = 0;
+
+int cal = 0;
+// int poz = 1;
+
+foreach(string line in lines)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        if (cal >= maxCal1)
+        {
+            maxCal3 = maxCal2;
+            maxCal2 = maxCal1;
+            maxCal1 = cal;
+           // maxPoz = poz;
+           
+        } else if (cal >= maxCal2)
+        {
+            maxCal3 = maxCal2;
+            maxCal2 = cal;
+        } else if (cal >= maxCal3)
+        {
+            maxCal3 = cal;
+        }
+
+        cal = 0;
+       // poz++;
+
+    } else
+    {
+        cal += Int32.Parse(line);
+    }
+
+
+
+}
+
+if (cal >= maxCal1)
+{
+    maxCal3 = maxCal2;
+    maxCal2 = maxCal1;
+    maxCal1 = cal;
+    // maxPoz = poz;
+
+}
+else if (cal >= maxCal2)
+{
+    maxCal3 = maxCal2;
+    maxCal2 = cal;
+}
+else if (cal >= maxCal3)
+{
+    maxCal3 = cal;
+}
+
+Console.WriteLine(maxPoz + " " + (maxCal1 + maxCal2 + maxCal3));
