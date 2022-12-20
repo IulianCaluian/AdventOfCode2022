e61b89b085566de73074f5ad082daff094bcdcb9
Iulian Caluian <iulian.coder@gmail.com>
2022-12-20 22:17:37 +0200

    day 6 part 2

--- AoC_Day6/Program.cs
+++ AoC_Day6/Program.cs
@@ -7,9 +7,12 @@ string[] lines = File.ReadAllLines("input.txt");
 string input = lines[0];
 
 int poz = 0;
-for(  ; poz < input.Length -3; poz++)
+for(  ; poz < input.Length -13; poz++)
 {
-    if (allDiferent(input[poz], input[poz + 1], input[poz + 2], input[poz + 3]))
+    //if (allDiferent(input[poz], input[poz + 1], input[poz + 2], input[poz + 3]))
+    //    break;
+
+    if (all14Diferent(input, poz))
         break;
 }
 
@@ -27,8 +30,23 @@ bool allDiferent(char v1, char v2, char v3, char v4)
     return true;
 }
 
+bool all14Diferent(string input, int offset)
+{
+    string subStr = input.Substring(offset, 14);
+
+    foreach(char c in subStr)
+    {
+     int  howManyCs = subStr.Where(x => x == c).Count();
+        if (howManyCs != 1)
+            return false;
+    }
+
+    return true;
+}
 
 
-Console.WriteLine(poz + 4);
+
+//Console.WriteLine(poz + 4);
+Console.WriteLine(poz + 14);
 
 
