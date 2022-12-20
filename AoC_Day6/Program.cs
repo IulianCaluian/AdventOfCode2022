629c25115eb0729090b2489b35e2f0b2aa110e47
Iulian Caluian <iulian.coder@gmail.com>
2022-12-20 22:11:26 +0200

    day 6 part 1

---
+++ AoC_Day6/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] lines = File.ReadAllLines("input.txt");
+
+
+string input = lines[0];
+
+int poz = 0;
+for(  ; poz < input.Length -3; poz++)
+{
+    if (allDiferent(input[poz], input[poz + 1], input[poz + 2], input[poz + 3]))
+        break;
+}
+
+bool allDiferent(char v1, char v2, char v3, char v4)
+{
+    if (v1 == v2 || v1 == v3 || v1 == v4)
+        return false;
+
+    if (v2 == v3 || v2 == v4)
+        return false;
+
+    if (v3 == v4)
+        return false;
+
+    return true;
+}
+
+
+
+Console.WriteLine(poz + 4);
+
+
