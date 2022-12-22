121607d511ec24c985152741b46900082085c2fb
Iulian Caluian <iulian.coder@gmail.com>
2022-12-22 20:53:58 +0200

    day 8 part 2

--- AoC_Day8/Program.cs
+++ AoC_Day8/Program.cs
@@ -107,3 +107,144 @@ for (int i = 0; i < height; i++)
 }
 
 Console.WriteLine(rez);
+
+
+
+// Part2:
+
+// Up:
+
+int[,] untilFirstBigColuumn = new int[height, 200]; // num of chars on the left lower or equal to c, where c is the second char.
+int[,] bestUp = new int[width, height];
+
+for (int i = 0; i < height; i++)
+    for (int j = 0; j < width; j++)
+    {
+        if (i == 0)
+        {
+            // ifLeft[j, k] = 
+
+            for (int k = '0'; k <= '9'; k++)
+            {
+                untilFirstBigColuumn[j, k] = 1;
+            }
+            continue;
+        }
+
+        char h = lines[i][j];
+
+        bestUp[i, j] = untilFirstBigColuumn[j,h];
+
+        for (int k = '0'; k <= '9'; k++)
+        {
+            if (k <= h)
+                untilFirstBigColuumn[j, k] = 1;
+            else
+                untilFirstBigColuumn[j, k] = untilFirstBigColuumn[j, k] + 1;
+        }
+    }
+
+
+int[,] bestDown = new int[width, height];
+
+for (int i = height - 1; i >= 0; i--)
+    for (int j = 0; j < width; j++)
+    {
+        if (i == height - 1)
+        {
+            // ifLeft[j, k] = 
+
+            for (int k = '0'; k <= '9'; k++)
+            {
+                untilFirstBigColuumn[j, k] = 1;
+            }
+            continue;
+        }
+
+        char h = lines[i][j];
+
+        bestDown[i, j] = untilFirstBigColuumn[j, h];
+
+        for (int k = '0'; k <= '9'; k++)
+        {
+            if (k <= h)
+                untilFirstBigColuumn[j, k] = 1;
+            else
+                untilFirstBigColuumn[j, k] = untilFirstBigColuumn[j, k] + 1;
+        }
+    }
+
+// best left:
+int[,] bestLeft = new int[width, height];
+int[,] untilFirstBigRow = new int[width, 200]; // num of chars on the left lower or equal to c, where c is the second char.
+for (int j = 0; j < width; j++)
+    for (int i = 0; i < height; i++)
+    {
+        if (j == 0)
+        {
+            // ifLeft[j, k] = 
+
+            for (int k = '0'; k <= '9'; k++)
+            {
+                untilFirstBigRow[i, k] = 1;
+            }
+            continue;
+        }
+
+        char h = lines[i][j];
+
+        bestLeft[i, j] = untilFirstBigRow[i, h];
+
+        for (int k = '0'; k <= '9'; k++)
+        {
+            if (k <= h)
+                untilFirstBigRow[i, k] = 1;
+            else
+                untilFirstBigRow[i, k] = untilFirstBigRow[i, k] + 1;
+        }
+    }
+
+int[,] bestRight = new int[width, height];
+for (int j = width - 1; j >= 0; j--)
+    for (int i = 0; i < height; i++)
+    {
+        if (j == width - 1)
+        {
+            // ifLeft[j, k] = 
+
+            for (int k = '0'; k <= '9'; k++)
+            {
+                untilFirstBigRow[i, k] = 1;
+            }
+            continue;
+        }
+
+        char h = lines[i][j];
+
+        bestRight[i, j] = untilFirstBigRow[i, h];
+
+        for (int k = '0'; k <= '9'; k++)
+        {
+            if (k <= h)
+                untilFirstBigRow[i, k] = 1;
+            else
+                untilFirstBigRow[i, k] = untilFirstBigRow[i, k] + 1;
+        }
+
+    }
+
+int max = 0;
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < width; j++)
+    {
+        int cost = (bestUp[i, j] * bestDown[i, j] * bestLeft[i, j] * bestRight[i, j]);
+        if (cost > max)
+            max = cost;
+
+    //    Console.Write( + " ");
+    }
+  //  Console.WriteLine("");
+}
+
+Console.WriteLine(max);
