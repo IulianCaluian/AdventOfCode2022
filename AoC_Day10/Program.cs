473436faa1c1158fdd843e6f05ef806ff292a1a9
Iulian Caluian <iulian.coder@gmail.com>
2022-12-28 18:53:08 +0200

    day 10

---
+++ AoC_Day10/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+string[] lines = File.ReadAllLines("input.txt");
+
+int X = 1;
+
+int cycle = 0;
+int iSig = 0;
+
+foreach (string line in lines)
+{
+    string[] parts = line.Split(' ');
+    string op = parts[0];
+
+    if (op == "noop")
+    {
+        cycle++;
+         CheckIfInterestingSignal(cycle, X);
+
+    } else if (op == "addx")
+    {
+        cycle++;
+        CheckIfInterestingSignal(cycle, X);
+        cycle++;
+        X += Int32.Parse(parts[1]);
+        CheckIfInterestingSignal(cycle, X);
+
+    }
+
+}
+
+
+int CheckIfInterestingSignal(int c, int x)
+{
+    int sig = 0;
+
+    if ((c - 20) % 40 == 0)
+        sig =  c * x;
+
+    Console.WriteLine(x);
+    Console.WriteLine($"{c} - {sig}");
+
+    return sig;
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,113 +1,81 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
 Console.WriteLine("Hello, World!");
+
+
+
 string[] lines = File.ReadAllLines("input.txt");
 
+int X = 1;
 
-
-SortedDictionary<int, SortedSet<int>> viz = new SortedDictionary<int, SortedSet<int>>();
-
-
-int moves;
-
-
-P[] rope = new P[10];
-
-viz.Add(0, new SortedSet<int> { 0 });
-
-foreach (var line in lines)
-{
-    string[] parts = line.Split(' ');
-
-    char dir = parts[0][0];
-    moves = Int32.Parse(parts[1]);
-
-    for (int i = 0; i < moves; i++)
-    {
-        if (dir == 'U') rope[0].X--;
-        if (dir == 'D') rope[0].X++;
-        if (dir == 'L') rope[0].Y--;
-        if (dir == 'R') rope[0].Y++;
-
-        for (int j = 1; j <= 9; j++)
-        {
-            rope[j] = CalculateNewTail(rope[j - 1], rope[j]);
-      //      Console.WriteLine($"{j}: {rope[j].X},{rope[j].Y}");
-        }
-     //   Console.WriteLine();
-
-        int tx = rope[9].X;
-        int ty = rope[9].Y;
-
-        if (!viz.ContainsKey(tx))
-            viz.Add(tx, new SortedSet<int>());
-
-        var row = viz[tx];
-        if (!row.Contains(ty))
-            row.Add(ty);
-
-
-
-
-    }
-
-
-
-}
-
+int cycle = 0;
+int iSig = 0;
 
 int rez = 0;
-foreach (var row in viz)
-{
-    rez += row.Value.Count();
-}
+char[,] crt = new char[6, 40];
 
+foreach (string line in lines)
+{
+    string[] parts = line.Split(' ');
+    string op = parts[0];
+
+    if (op == "noop")
+    {
+        cycle++;
+        iSig = CheckIfInterestingSignal(cycle, X);
+        rez += iSig;
+    }
+    else if (op == "addx")
+    {
+        cycle++;
+        iSig = CheckIfInterestingSignal(cycle, X);
+        rez += iSig;
+        cycle++;
+        
+   
+        iSig = CheckIfInterestingSignal(cycle, X);
+        rez += iSig;
+
+        X += Int32.Parse(parts[1]);
+
+    }
+
+}
 Console.WriteLine(rez);
 
-
-P CalculateNewTail(P H, P oldT)
+for (int ro = 0; ro < 6; ro++)
 {
-    P newT = new P();
-    newT.X = oldT.X;
-    newT.Y = oldT.Y;
-
-    // Move tail:
-    // Move tail:
-    if (Math.Abs(H.Y - oldT.Y) == 2 && Math.Abs(H.X - oldT.X) == 2)
-    {
-        newT.X = (H.X + oldT.X) / 2;
-        newT.Y = (H.Y + oldT.Y) / 2;
-    }
-    else if (Math.Abs(H.X - oldT.X) == 2)
-    {
-        newT.X = (H.X + oldT.X) / 2;
-        newT.Y = H.Y;
-    }
-    else if (Math.Abs(H.Y - oldT.Y) == 2)
-    {
-        newT.X = H.X;
-        newT.Y = (H.Y + oldT.Y) / 2;
-    }
-
-    return newT;
-}
-
-public struct P
-{
-    public int X;
-    public int Y;
-
-    public P()
-    {
-        X = Y = 0;
-    }
-
+    for (int co = 0; co < 40; co++)
+       if (crt[ro,co] == '#')
+        Console.Write(crt[ro,co]);
+    else
+            Console.Write('.');
+    Console.WriteLine();
 }
 
 
+int CheckIfInterestingSignal(int c, int x)
+{
+    int sig = 0;
+
+    if ((c - 20) % 40 == 0 && c <= 220)
+    {
+        sig = c * x;
+
+        Console.WriteLine($"{c}*{x}");
+    }
 
 
+    if ( c <= 240)
+    {
+        int row = ((c -1) / 40 );
+        int col = ((c - 1) % 40);
 
+        if (x - 1 <= (col ) && (col ) <= x + 1)
+            crt[row, col] = '#';
+    }
 
+  //  Console.WriteLine(x);
+  //  Console.WriteLine($"{c} - {sig}");
 
+    return sig;
+}
